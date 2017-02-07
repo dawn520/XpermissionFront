e978d6e276d90bf1a3f66a35382f53268e643c8d
xixi zhou <admin@zcx.win>
2017-02-07 12:38:03 +0800

    Initial commit

---
+++ src/page/order.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <h1>
+                跌幅高达日豆腐干而
+                <small>是打发斯蒂芬 问题</small>
+            </h1>
+            <ol class="breadcrumb">
+                <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
+                <li class="active">Here</li>
+            </ol>
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+
+            <!-- Your Page Content Here -->
+
+        </section>
+        <!-- /.content -->
+    </div>
+</template>
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+/**
+ * Created by zhouchenxi on 2017/2/3.
+ */
+var path = require('path');
+module.exports = {
+    entry: {
+        index:'./src/main.js'
+    },
+    //定义webpack输出的文件，我们在这里设置了让打包后生成的文件放在dist文件夹下的build.js文件中
+    output: {
+        path: '/dist',
+        publicPath: '/dist/',
+        filename: 'build.js'
+    },
+    module: {
+        //加载器配置
+        loaders: [
+            {test: /\.vue$/, loader: 'vue-loader',},
+            //.css 文件使用 style-loader 和 css-loader 来处理
+            {test: /\.css$/, loader: 'style-loader!css-loader'},
+            //转化ES6语法
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {presets: ['es2015']}
+            },
+            //.scss 文件使用 style-loader、css-loader 和 sass-loader 来编译处理
+            {test: /\.scss$/, loader: 'style!css!sass?sourceMap'},
+            //图片文件使用 url-loader 来处理，小于8kb的直接转为base64
+            {test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'}
+        ]
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.common.js'
+        },
+        extensions: ['*','.js', '.vue','.css']
+    }
+}
